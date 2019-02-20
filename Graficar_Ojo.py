5977e9359db7b6f4a930085484d9ff16e81005ce
comdig1 <comdiguis@saber.uis.edu.co>
2019-02-20 03:03:56 -0500

    nuevas ayudas para graficar diagrama de ojo con varios valores de BW

--- Graficar_Ojo.py
+++ Graficar_Ojo.py
@@ -1,8 +1,8 @@
 # NOTA IMPORTANTE
 # para correr este programa es necesario: 
-# 1) tener instalada la librería de Diagrama de Ojo, la cual se encuentra en este 
+# 1) tener instalada la libreria de Diagrama de Ojo, la cual se encuentra en este 
 # enlace: https://github.com/hortegab/Diagrama-de-ojo-Comunicaciones-II.git
-# 2) En el archivo Senal hay unos datos por defecto para graficar el diagrama de ojo, pero este archivo se puede actualizar corriendo el flujograma adjunto no sin antes activaro el bloque "File Sink" que envia datos al archivo.
+# 2) En el archivo Senal hay unos datos por defecto para graficar el diagrama de ojo, pero este archivo se puede actualizar corriendo el flujograma adjunto no sin antes activar el bloque "File Sink" que envia datos al archivo.
 
 
 from eyediagram.mpl import eyediagram
@@ -17,7 +17,7 @@ Ndat=10000
 Sps = 16 
 
 # Leemos el archivo producido por gnuradio
-f=scipy.fromfile(open("Senal"), dtype=scipy.float32) 
+f=scipy.fromfile(open("Senal1"), dtype=scipy.float32) 
 
 #Escogemos una porcion de datos para graficar
 fp=(f[0:Ndat])
@@ -27,4 +27,47 @@ fp=(f[0:Ndat])
 eyediagram(fp, 2*Sps, offset=16, cmap=plt.cm.coolwarm)
 plt.show()
 
+# Leemos el archivo producido por gnuradio
+f=scipy.fromfile(open("Senal2"), dtype=scipy.float32) 
+
+#Escogemos una porcion de datos para graficar
+fp=(f[0:Ndat])
+
+#Grafica para Rectangular
+# plt.subplot(221)
+eyediagram(fp, 2*Sps, offset=16, cmap=plt.cm.coolwarm)
+plt.show()
+
+# Leemos el archivo producido por gnuradio
+f=scipy.fromfile(open("Senal3"), dtype=scipy.float32) 
+
+#Escogemos una porcion de datos para graficar
+fp=(f[0:Ndat])
+
+#Grafica para Rectangular
+# plt.subplot(221)
+eyediagram(fp, 2*Sps, offset=16, cmap=plt.cm.coolwarm)
+plt.show()
+
+# Leemos el archivo producido por gnuradio
+f=scipy.fromfile(open("Senal4"), dtype=scipy.float32) 
+
+#Escogemos una porcion de datos para graficar
+fp=(f[0:Ndat])
+
+#Grafica para Rectangular
+# plt.subplot(221)
+eyediagram(fp, 2*Sps, offset=16, cmap=plt.cm.coolwarm)
+plt.show()
+
+# Leemos el archivo producido por gnuradio
+f=scipy.fromfile(open("Senal5"), dtype=scipy.float32) 
+
+#Escogemos una porcion de datos para graficar
+fp=(f[0:Ndat])
+
+#Grafica para Rectangular
+# plt.subplot(221)
+eyediagram(fp, 2*Sps, offset=16, cmap=plt.cm.coolwarm)
+plt.show()
 
